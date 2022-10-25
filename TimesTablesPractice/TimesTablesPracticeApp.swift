e7f3b0edd69bfc3ab34e76ecae5f3eb61bc06ecd
Matilda Mared <matildamared@live.se>
2022-10-25 13:53:02 +0200

    Initial Commit

---
+++ TimesTablesPractice/TimesTablesPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesPracticeApp.swift
+//  TimesTablesPractice
+//
+//  Created by Matilda Mared on 2022-10-25.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
