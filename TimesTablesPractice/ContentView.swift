ea75c5837aca18db45d4a2d9f2a45ebf6c083724
Matilda Mared <matildamared@live.se>
2022-10-25 15:33:11 +0200

    add styling

--- TimesTablesPractice/ContentView.swift
+++ TimesTablesPractice/ContentView.swift
@@ -11,10 +11,29 @@ enum GameStatus {
     case Initial, Running, Completed
 }
 
-struct ContentView: View {
+extension View {
+    func buttonStyles() -> some View {
+        modifier(StyledButton())
+    }
+}
+
+struct StyledButton: ViewModifier {
     let yellowColor = Color(hue: 0.121, saturation: 1.0, brightness: 0.939, opacity: 1.0)
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .foregroundColor(.white)
+            .background(yellowColor)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+
+struct ContentView: View {
     @State private var scaleEffect = 0.5
     @State private var gameStatus = GameStatus.Initial
+    @State private var timesTableToPractice = 1
+    @State private var numberOfQuestions = 5
     
     var body: some View {
         ZStack {
@@ -43,6 +62,7 @@ struct ContentView: View {
             Button("Reset Game") {
                 resetGame()
             }
+            .buttonStyles()
         }
     }
     
@@ -66,13 +86,28 @@ struct ContentView: View {
                 .padding()
                 .fontWeight(.light)
             
+            List {
+                Picker("Times table", selection: $timesTableToPractice) {
+                    ForEach(1..<13) {
+                        Text("\($0)")
+                    }
+                }
+                .listRowBackground(Color.white.opacity(0.5))
+                
+                Picker("Number of questions", selection: $numberOfQuestions) {
+                    Text("5").tag(5)
+                    Text("10").tag(10)
+                    Text("15").tag(15)
+                    Text("20").tag(20)
+                }
+                .listRowBackground(Color.white.opacity(0.5))
+            }
+            .scrollContentBackground(.hidden)
+            
             Button("Practice") {
                 startGame()
             }
-                .padding()
-                .foregroundColor(.white)
-                .background(yellowColor)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .buttonStyles()
             
             Spacer()
             
